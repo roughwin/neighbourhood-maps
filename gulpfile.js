dd8e124f8edf2301e31a7fcff6c3d8c7795b0dd1
roughwin <roughwin@qq.com>
2017-03-28 16:50:49 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,58 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+const deleteLines = require('gulp-delete-lines')
+const minify = require('gulp-minifier')
 
-gulp.task("test", function(){
-    console.log('helo');
+var src_option = {
+    base: './src/',
+}
+var ignore_path = ['!node_modules',
+    '!gulpfile*',
+    '!node_modules/**/*',
+    '!dist',
+    '!dist/**/*',
+    '!./*.@[js|json|md|css]',
+    '!./src/jasmine',
+    '!./src/jasmine/**/*'];
+var path = {
+    img: ['./src/**/*.@(jpg|png)'].concat(ignore_path),
+    js: ['./src/**/*.js'].concat(ignore_path),
+    css: ['./src/**/*.css'].concat(ignore_path),
+    html: ['./src/**/*.html'].concat(ignore_path),
+}
+
+gulp.task('test',function() {
+    console.log('hello gulp.')
+})
+
+gulp.task('output', function() {
+    gulp.src(['./src/**/*.+(jpg|png|eot|svg|ttf|woff)'].concat(ignore_path),src_option)
+        .pipe(gulp.dest('dist'))
+    gulp.src(['./src/**/*.+(html|css|js)'].concat(ignore_path),src_option)
+        .pipe(deleteLines({
+            'filters': [
+                /.jasmine./i
+            ]
+        }))
+        .pipe(minify({
+            minify: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            minifyJS: true,
+            minifyCSS: true,
+         }))
+        .pipe(gulp.dest('dist'))
+})
+gulp.task('ser', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+
+    gulp.watch(['./src/**/*+(html|js|css)'].concat(ignore_path)).on('change',function () {
+        reload();
+    })
 })
