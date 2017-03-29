874b4582f2287869ffcc3dab4ebe2794a0a02d3c
roughwin <roughwin@qq.com>
2017-03-29 11:26:06 +0800

    地图 坐标 加载实现

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,47 @@
-console.log('test')
+var w = window;
+function initMap() {
+    w.map = new google.maps.Map(document.getElementById('map'), {
+        center:  {lat: 30.284863403785405, lng: 120.11222183704376},
+        zoom: 15
+    });
+    map.addListener('click', function(e) {
+        // placeMarkerAndPanTo(e.latLng, map);
+        console.log({lat:e.latLng.lat(),lng:e.latLng.lng()})
+    });
 
+    drawmark();
+}
 
+function drawmark() {
+    var mark =  {lat: 30.284863403785405, lng: 120.11222183704376}
+    window.markers = [];
+    var request = {
+        location: {lat: 30.284863403785405, lng: 120.11222183704376},
+        radius: '500',
+        query: 'bus stop'
+    };
+    var service = new google.maps.places.PlacesService(w.map);
+    service.textSearch(request, function(result, status){
+        // var re = result.map(function(e,i){
+        //     return {lat: e.geometry.location.lat(), lng: e.geometry.location.lng()}
+        // })
+        window.result = result;
+        result.forEach(function(e,i) {
+            var marker = new google.maps.Marker({
+                position: {lat: e.geometry.location.lat(), lng: e.geometry.location.lng()},
+                map: w.map,
+                title: e.name
+            })
+            markers.push(marker)
+            console.log(e);
+        })
+        $('#address').val(markers.length);
+    });
+    // var marker = new google.maps.Marker({
+    //     position: mark,
+    //     map: map,
+    //     title: 'title test'
+    // })
 
-
+}
 //AIzaSyAGnhR_0_6K7xOjz3Jy15uoz06JlTi6mME
